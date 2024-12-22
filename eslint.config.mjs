7ffe32cd153c914c8978d7194fd06eb7e2f2125d
Philip Allotey <77976603+alloteyphil@users.noreply.github.com>
2024-12-22 22:14:02 +0000

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,9 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-    },
+    rules: {},
   }),
 ];
 
